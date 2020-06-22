470043671e73ce6ecdc35466b426730cc5bedda8
jacobhsu <jacob.hsu.tw@gmail.com>
2020-06-22 10:41:58 +0800

    Updates

---
+++ precache-manifest.469d0b53d31d2b639ba62f4d974724d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044fcd226688a99b0052d41eeb36ebde",
+    "url": "/sku-react/index.html"
+  },
+  {
+    "revision": "979af23984a96cf5d871",
+    "url": "/sku-react/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "1e74e19180e6e951dc84",
+    "url": "/sku-react/static/css/main.50252109.chunk.css"
+  },
+  {
+    "revision": "979af23984a96cf5d871",
+    "url": "/sku-react/static/js/2.13c66261.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sku-react/static/js/2.13c66261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e74e19180e6e951dc84",
+    "url": "/sku-react/static/js/main.ac188c2b.chunk.js"
+  },
+  {
+    "revision": "00a816c34ab4a0c6fb05",
+    "url": "/sku-react/static/js/runtime-main.0601b98f.js"
+  }
+]);
